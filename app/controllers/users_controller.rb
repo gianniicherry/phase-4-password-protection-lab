2ce60d255b63ead63afb8e41b2e12c11417dbfa1
Gianni <gianniicherry@gmail.com>
2023-06-20 10:36:37 -0700

    commit tree 63d34b42557d169c72654710fabb495ecb67940e

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+    wrap_parameters format: []
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity(invalid)
+
+    def create
+        user = User.create!(user_params)
+        render json: user
+    end
+
+    private
+    def render_unprocessable_entity(invalid)
+        render json:{error: invalid.record.errors}, status: :unprocessable_entity
+    end
+
+    def user_params
+        params.permit(:username,:password)
+    end
+end
